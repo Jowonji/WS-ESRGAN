323d7e31bc69a5776929e40d3ef15d2a0fd17aaf
Jowonji <wonji1283@naver.com>
2024-08-17 17:02:34 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -38,21 +38,24 @@ def display_transform():
 
 
 class TrainDatasetFromFolder(Dataset):
-    def __init__(self, dataset_dir, crop_size, upscale_factor):
+    def __init__(self, lr_dir, hr_dir):
         super().__init__()
-        self.image_filenames = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image_file(x)]
-        self.hr_transform = train_hr_transform(crop_size)
-        self.lr_transform = train_lr_transform(crop_size, upscale_factor)
+        self.lr_image_filenames = [join(lr_dir, x) for x in listdir(lr_dir) if is_image_file(x)]
+        self.hr_image_filenames = [join(hr_dir, x) for x in listdir(hr_dir) if is_image_file(x)]
 
     def __getitem__(self, index):
-        hr_image = self.hr_transform(Image.open(self.image_filenames[index]))
-        lr_image = self.lr_transform(hr_image)
+        # 4채널 이미지를 3채널로 변환
+        lr_image = Image.open(self.lr_image_filenames[index]).convert('RGB')
+        hr_image = Image.open(self.hr_image_filenames[index]).convert('RGB')
+
+        lr_image = ToTensor()(lr_image)
+        hr_image = ToTensor()(hr_image)
         return lr_image, hr_image
     
     def __len__(self):
-        return len(self.image_filenames)
-    
+        return len(self.lr_image_filenames)
 
+    
 class ValDatasetFromFolder(Dataset):
     def __init__(self, dataset_dir, upscale_factor):
         super().__init__()
